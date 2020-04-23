844b4c3de872694c003ba279f081daae9eb9ca31
Patrick Nyatindo <nyatindopatrick@gmail.com>
2020-04-23 11:53:04 +0300

    reset server

--- src/server/routes/index.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-
-router.use('/admin', require('./admin'));
-router.use('/sacco', require('./sacco'));
-router.use('/rider', require('./rider'));
-router.use('/sms', require('./sms'));
-
-module.exports = router;
--- src/server/routes/sacco/index.js
+++
@@ -1,7 +0,0 @@
-const router = require('express').Router();
-const saccoRoutes = require('../../controllers/Sacco');
-const { sacco, riders } = saccoRoutes;
-router.post('/', sacco);
-
-router.post('/riders', riders);
-module.exports = router;
